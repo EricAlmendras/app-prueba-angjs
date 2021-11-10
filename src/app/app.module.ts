0a5b70934f5648af0fe669911ce7ba566d0c0f0d
Eric Almendras <ericalmendras@hotmail.com>
2021-11-09 22:45:43 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimercomponentComponent } from './primercomponent/primercomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimercomponentComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/primercomponent/primercomponent.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrimercomponentComponent } from './primercomponent.component';
+
+describe('PrimercomponentComponent', () => {
+  let component: PrimercomponentComponent;
+  let fixture: ComponentFixture<PrimercomponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PrimercomponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrimercomponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
